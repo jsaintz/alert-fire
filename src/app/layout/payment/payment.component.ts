2ba4b12af04ffd4f7adf61424bf739a14791f58f
Jonatas Ernesto Silva Dos Santos <jonatas.santos@avanade.com>
2019-10-20 12:47:54 -0300

    Payment

--- src/app/layout/payment/payment.component.ts
+++ src/app/layout/payment/payment.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { routerTransition } from "src/app/router.animations";
 
 @Component({
-    selector: 'app-payment',
-    templateUrl: './payment.component.html',
-    styleUrls: ['./payment.component.scss']
+    selector: "app-payment",
+    templateUrl: "./payment.component.html",
+    styleUrls: ["./payment.component.scss"],
+    animations: [routerTransition()]
 })
 export class PaymentComponent implements OnInit {
-    ngOnInit(): void {
-        throw new Error("Method not implemented.");
-    }
+    ngOnInit() {}
 
     constructor() {}
-
-   
 }
